c62917a3fa7fe4928a817a4dc73eaccffa9c8d69
JaimeMatias <matias.raul.jaime@gmail.com>
2019-07-04 16:14:19 -0300

    TDD con A

--- test_decision.py
+++ test_decision.py
@@ -2,12 +2,17 @@ from comparar import comparar
 import unittest
 import os
 class  TestStringMethods(unittest.TestCase ):
-    def test_complejidad(self):
+    def test_complejidad_decision(self):
         var = os.system('xenon -b A decision.py')
         valor= 0
 
         self.assertEqual(var, valor)
 
+    def test_complejidad_Read(self):
+        var = os.system('xenon -b B Read.py')
+        valor = 0
+
+        self.assertEqual(var, valor)
 
 if __name__ == '__main__':
     unittest.main()
